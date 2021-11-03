e1eb7d21a1a829840bafe2871be673c4279acf39
Rene Arnhold <rene.arnhold@racs-net.de>
2021-11-03 15:16:04 +0100

    first release

---
+++ src/config.h
@@ -0,0 +1,19 @@
+#define DHT22_01
+#define LORA_CLK_ERR        2
+#define INDICATE_LED        10
+
+#define DHTPIN              7
+#define DHTENPIN            4
+
+#define DHT_ERR_CHECKSUM    0xA0
+#define DHT_ERR_TIMEOUT     0xA1
+#define DHT_ERR_INVALID     0xA2
+#define DHT_ERR_UNKNOWN     0xA3
+
+#define BAT_MAX             630
+#define BAT_MIN             530
+
+#define PINMODEPOWERSAVE
+#define DISABLEBOD
+#define LINKCHECKMODE
+#define ADRMODE
